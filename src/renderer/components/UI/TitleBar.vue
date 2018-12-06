cd8e30843212e1706cd11e4edd5740fe8725d548
Buds <mrbouyou@gmail.com>
2018-12-06 07:33:46 +0100

    TitleBar darker

--- src/renderer/components/UI/TitleBar.vue
+++ src/renderer/components/UI/TitleBar.vue
@@ -36,7 +36,7 @@ export default {
   display: flex;
   width: 100%;
   height: 32px;
-  background: #333;
+  background: #1a1919;
   user-select: none;
   cursor: default;
   -webkit-app-region: drag;
