c178720397cd700382ed18b1515fc00dc2b76180
Nightwarden24 <CodeNightwarden24@gmail.com>
2018-12-12 07:20:07 +0300

    Extend plural rules
    Implement the plural rule №7 by overriding the `VueI18n.prototype.getChoiceIndex` function so that pluralization works in Russian.
    See more:
    https://developer.mozilla.org/en-US/docs/Mozilla/Localization/Localization_and_Plurals
    http://kazupon.github.io/vue-i18n/guide/pluralization.html#custom-pluralization

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -21,6 +21,43 @@ Vue.config.productionTip = false;
 
 Vue.use(VueI18n);
 
+const defaultPluralRules = VueI18n.prototype.getChoiceIndex;
+
+/**
+ * @param choice {number} a choice index given by the input to $tc: `$tc('path.to.rule', choiceIndex)`
+ * @param choicesLength {number} an overall amount of available choices
+ * @returns a final choice index to select plural word by
+ */
+VueI18n.prototype.getChoiceIndex = function extendedPluralRules(
+  choice,
+  choicesLength
+) {
+  // this === VueI18n instance, so the locale property also exists here
+  if (this.locale !== "ru") {
+    // proceed to the default implementation
+    return defaultPluralRules.apply(this, [choice, choicesLength]);
+  }
+  const aChoice = Math.abs(choice);
+
+  if (aChoice === 0) {
+    return 0;
+  }
+
+  const withoutHundreds = aChoice % 100;
+  const teen = withoutHundreds > 10 && withoutHundreds < 20;
+  const endsWithOne = withoutHundreds % 10 === 1;
+
+  if (!teen && endsWithOne) {
+    return 1;
+  }
+
+  if (!teen && withoutHundreds % 10 >= 2 && withoutHundreds % 10 <= 4) {
+    return 2;
+  }
+
+  return choicesLength < 4 ? 2 : 3;
+};
+
 const i18n = new VueI18n({
   locale: "en",
   fallbackLocale: "en",
