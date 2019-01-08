4057490812ef766a5ef409bcc4194c31bbc03f68
Buds <mrbouyou@gmail.com>
2019-01-08 20:03:58 +0100

    fix needUpdate function

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1011,32 +1011,36 @@ local versionTarget = "2.11.0-beta2"
 if not WeakAuras.versionString then return end
 
 local function needUpdate(actual, target)
-  if actual == target then return false end
-
-  local function splitByDot(str)
-    str = str or ""
-    local t, count = {}, 0
-    str:gsub("([^%.%-]+)", function(c)
-      count = count + 1
-      t[count] = c
-    end)
-    return t
-  end
-  
-  actual = splitByDot(actual)
-  target = splitByDot(target)
-  
-  local c = 1
-  while target[c] do
-    if not actual[c] then
-      return false
+    if actual == target then return false end
+    
+    local function splitByDot(str)
+        str = str or ""
+        local t, count = {}, 0
+        str:gsub("([^%.%-]+)", function(c)
+                count = count + 1
+                t[count] = c
+        end)
+        return t
     end
-    if actual[c] < target[c] then
-      return true
+    
+    actual = splitByDot(actual)
+    target = splitByDot(target)
+    
+    local c = 1
+    while target[c] do
+        if not actual[c] then
+            return false
+        end
+        if actual[c] ~= target[c] then
+            if tonumber(actual[c]) ~= nil and tonumber(target[c]) ~= nil then
+                return tonumber(actual[c]) < tonumber(target[c])
+            else
+                return actual[c] < target[c]
+            end
+        end
+        c = c + 1
     end
-    c = c + 1
-  end
-  return false
+    return true
 end
 
 if needUpdate(WeakAuras.versionString, versionTarget) then
