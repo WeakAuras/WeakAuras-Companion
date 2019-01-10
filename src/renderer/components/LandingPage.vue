71eb3d2ccecc90e5f279f2f2fcb7b490d1456eaa
Buds <mrbouyou@gmail.com>
2019-01-10 20:48:18 +0100

    WA version check was still loading data

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1068,6 +1068,7 @@ end
 
 if needUpdate(WeakAuras.versionString, versionTarget) then
   WeakAuras.prettyPrint(("WeakAuras Companion requires WeakAuras version >= %s"):format(versionTarget))
+  WeakAurasCompanion = nil
   return
 else
   WeakAuras.prettyPrint("WeakAuras Companion is in beta stage, we do not advise to use it yet, and if you do join https://discord.gg/wa2 #companion-app")
