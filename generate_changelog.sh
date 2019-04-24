a84cae8fc2f0774e9379afec4ef2e3510092954f
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2019-04-24 22:59:01 +0200

    Update appveyor.yml

---
+++ generate_changelog.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+version=$( git describe --tags --always )
+tag=$( git describe --tags --always --abbrev=0 )
+
+if [ "$version" = "$tag" ]; then # on a tag
+  current="$tag"
+  previous=$( git describe --tags --abbrev=0 HEAD~ )
+  if [[ $previous == *beta* ]]; then
+    if [[ $tag == *beta* ]]; then
+      previous=$( git describe --tags --abbrev=0 HEAD~ )
+    else
+      previous=$( git describe --tags --abbrev=0 --exclude="*beta*" HEAD~ )
+    fi
+  else
+    previous=$( git describe --tags --abbrev=0 HEAD~ )
+  fi
+else
+  current=$( git log -1 --format="%H" )
+  previous="$tag"
+fi
+
+date=$( git log -1 --date=short --format="%ad" )
+url=$( git remote get-url upstream | sed -e 's/^git@\(.*\):/https:\/\/\1\//' -e 's/\.git$//' )
+
+echo -ne "# [${version}](${url}/tree/${current}) ($date)\n\n[Full Changelog](${url}/compare/${previous}...${current})\n\n" > "CHANGELOG.md"
+
+if [ "$version" = "$tag" ]; then # on a tag
+  highlights=$( git cat-file -p $tag | sed -e '1,5d' )
+  echo -ne "## Highlights\n\n ${highlights} \n\n## Commits\n\n" >> "CHANGELOG.md"
+fi
+
+git shortlog --no-merges --reverse "$previous..$current" | sed -e  '/^\w/G' -e 's/^      /- /' >> "CHANGELOG.md"
