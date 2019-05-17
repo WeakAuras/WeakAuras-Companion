e665b43dce69de390e7d5becff1d4a46306a95cb
Buds <mrbouyou@gmail.com>
2019-05-17 21:36:58 +0200

    show nag on ADDON_LOADED instead of PLAYER_ENTERING_WORLD

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1177,11 +1177,13 @@ if waBuildTime and waBuildTime < buildTimeTarget then
   WeakAurasCompanion = nil
 else
   local loadedFrame = CreateFrame("FRAME")
-  loadedFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-  loadedFrame:SetScript("OnEvent", function()
-    local count = WeakAuras.CountWagoUpdates()
-    if count and count > 0 then
-      WeakAuras.prettyPrint(WeakAuras.L["There are %i updates to your auras ready to be installed!"]:format(count))
+  loadedFrame:RegisterEvent("ADDON_LOADED")
+  loadedFrame:SetScript("OnEvent", function(_, _, addonName)
+    if addonName == "WeakAurasCompanion" then
+      local count = WeakAuras.CountWagoUpdates()
+      if count and count > 0 then
+        WeakAuras.prettyPrint(WeakAuras.L["There are %i updates to your auras ready to be installed!"]:format(count))
+      end
     end
   end)
 end`
