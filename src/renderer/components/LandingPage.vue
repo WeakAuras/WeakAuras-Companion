a0af04f5f591ca1399b89f084a1cf0ae99a0df57
Buds <mrbouyou@gmail.com>
2019-05-28 00:58:21 +0200

    fix reset toast and some translations

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -257,7 +257,6 @@ const defaultValues = {
   stash: [], // list of auras pushed from wago to wow with "SEND TO WEAKAURAS COMPANION APP" button
   reloadToast: null,
   updateToast: null,
-  settingsWipedToast: null,
   updater: {
     status: null, // checking-for-update, update-available, update-not-available, error, download-progress, update-downloaded
     progress: null,
@@ -447,22 +446,6 @@ export default Vue.extend({
         }
       }
     });
-    this.$electron.ipcRenderer.on("settings-wiped", () => {
-      console.log("hi from receiver");
-      this.settingsWipedToast = this.message(
-        this.$t(
-          "app.main.settingswiped" /* The Companion's Settings have been reset! */
-        ),
-        null,
-        {
-          className: "update update-error",
-          duration: 10,
-          onComplete: () => {
-            this.settingsWipedToast = null;
-          }
-        }
-      );
-    });
     this.$electron.ipcRenderer.on("updaterHandler", (event, status, arg) => {
       console.log(`updaterHandler: ${status}`);
       if (status === "checkForUpdates") {
@@ -620,8 +603,12 @@ export default Vue.extend({
       wowDefaultPath().then(value => {
         this.config.wowpath.value = value;
       });
-      this.$electron.ipcRenderer.send("settings-wiped");
-      console.log("Settings wiped");
+      this.message(
+        this.$t(
+          "app.main.settingswiped" /* Companion's settings have been reset! */
+        ),
+        "info"
+      );
     },
     open(link) {
       this.$electron.shell.openExternal(link);
