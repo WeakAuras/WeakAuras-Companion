fa315405c17662a3d0d0afd93999b6878512a6ce
Benjamin Staneck <staneck@gmail.com>
2020-01-15 00:04:02 +0100

    Update TOC for Patch 8.3.0

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1286,7 +1286,7 @@ export default Vue.extend({
         /* if (this.stash.lenghth > 0) { LuaOutput += "" } */
         const toc =
           AddonFolder.toLowerCase().search("classic") === -1
-            ? "80205"
+            ? "80300"
             : "11303";
         const files = [
           {
