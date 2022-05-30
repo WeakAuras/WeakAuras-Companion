3fd4dd63937c6fb6b0f7c46e83fc98db6496f170
Benjamin Staneck <staneck@gmail.com>
2022-05-30 07:51:36 +0200

    Initial conversion
    Nothing works

---
+++ packages/renderer/src/components/UI/Message.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="message">
+    <a
+      v-if="message.url"
+      :href="message.url"
+      :title="message.url"
+      target="_blank"
+      class="url"
+      v-html="message.text"
+    ></a>
+    <span v-else v-html="message.text"></span>
+    <Button
+      :type="message.type"
+      :title="message.time"
+      size="mini"
+      v-html="message.type"
+    ></Button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+
+export default defineComponent({
+  name: "Message",
+  components: { Button },
+  props: ["message"],
+});
+</script>
+
+<style lang="scss">
+.message {
+  text-align: center;
+  font-size: 11px;
+  margin-top: 2px;
+}
+
+.message span {
+  margin: 0 5px;
+}
+</style>
---
+++ packages/renderer/src/libs/user-data-folder.ts
@@ -0,0 +1,7 @@
+import { ipcRenderer } from "electron";
+
+const userDataPath = ipcRenderer.sendSync("get-user-data-path");
+
+console.log("userDataPath", userDataPath)
+
+export default userDataPath;
