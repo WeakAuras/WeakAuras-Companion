3a9d6045b328fdc7785436362fbc032c209b4057
Benjamin Staneck <staneck@gmail.com>
2023-01-16 03:03:22 +0100

    restructure project

---
+++ src/components/UI/Help.vue
@@ -0,0 +1,225 @@
+<template>
+  <div id="help" ref="help">
+    <div class="title">{{ $t("app.help.title" /* Setup Steps */) }}</div>
+    <ol class="follow">
+      <li>
+        {{
+          $t(
+            "app.help.s1" /* Configure WeakAuras Companion, you will need to at least set your World of Warcraft folder and account. */
+          )
+        }}
+      </li>
+      <li>
+        {{
+          $t(
+            "app.help.s2" /* Click on 'Fetch Updates', this is also done automatically once every hour. */
+          )
+        }}
+        <ol class="follow">
+          <li>
+            {{
+              $t(
+                "app.help.s2_1" /* Companion creates a list of your auras that contain a link to Wago. */
+              )
+            }}
+          </li>
+          <li>
+            {{ $t("app.help.s2_2" /* Companion sends the list to Wago. */) }}
+          </li>
+          <li>
+            {{
+              $t(
+                "app.help.s2_3" /* Wago sends updated import strings and Companion compares the versions. */
+              )
+            }}
+          </li>
+          <li>
+            {{
+              $t(
+                "app.help.s2_4" /* Companion creates a new "WeakAurasCompanion" addon automatically, this is where new import strings are saved. */
+              )
+            }}
+          </li>
+          <li>
+            {{ $t("app.help.s2_5" /* Reload your UI. */) }}
+          </li>
+        </ol>
+      </li>
+      <li>
+        {{
+          $t(
+            "app.help.s3" /* The first time you start WeakAuras Companion, you will have to reload your UI for it to be able to load the addon. */
+          )
+        }}
+      </li>
+      <li class="reset">
+        {{
+          $t(
+            "app.help.s4_1" /* Update your auras in-game by clicking the update button next to them in the WeakAuras options. */
+          )
+        }}
+        <br />
+        <img src="../../assets/ingame.png" class="center" />
+        <br />
+      </li>
+      <li>
+        {{
+          $t(
+            "app.help.s5" /* You can always skip a version or just ignore updates completely. */
+          )
+        }}
+        <br />
+        <img src="../../assets/ingame2.png" class="center" />
+      </li>
+    </ol>
+    <br />
+    <br />
+    <div class="title">
+      {{ $t("app.help.auraAuthorsTitle" /* Tips for Authors */) }}
+    </div>
+    <div class="paragraph">
+      {{
+        $t(
+          "app.help.auraAuthorsText1" /* If you publish auras on Wago, we recommend these settings: */
+        )
+      }}
+      <ul>
+        <li>
+          {{
+            $t(
+              "app.help.auraAuthorsText2" /* In the Companion app, add your Wago username in the "Wago Account" setting. */
+            )
+          }}
+        </li>
+        <li>
+          {{
+            $t(
+              "app.help.auraAuthorsText3" /* In WeakAuras, select the "Ignore all Updates" option for any aura you've modified heavily. */
+            )
+          }}
+        </li>
+      </ul>
+      <br />
+      <br />
+    </div>
+    <div class="title">{{ $t("app.help.foundbug" /* Found a bug? */) }}</div>
+    <div class="paragraph">
+      {{
+        $t(
+          "app.help.bug" /* If you encounter a bug, please create a ticket on */
+        )
+      }}
+      <a
+        target="_blank"
+        href="https://github.com/WeakAuras/WeakAuras-Companion/issues/new?template=bug_report.md"
+      >
+        GitHub
+      </a>
+      {{ $t("app.help.bugend" /* containing: */) }}
+      <br />
+      <ul>
+        <li>
+          {{ $t("app.help.bug.s1" /* A description of your problem. */) }}
+        </li>
+        <li>{{ $t("app.help.bug.s2" /* The steps to reproduce it. */) }}</li>
+        <li>
+          {{
+            $t(
+              "app.help.bug.s3" /* A screenshot, if it helps to understand your issue. */
+            )
+          }}
+        </li>
+        <li>
+          {{
+            $t(
+              "app.help.bug.s4" /* Error logs, type Ctrl+Shift+I to access the console and copy any error that might show up. */
+            )
+          }}
+        </li>
+      </ul>
+    </div>
+    <br />
+    <br />
+    <div class="title">{{ $t("app.help.support.title" /* Support */) }}</div>
+    <div class="paragraph">
+      {{
+        $t(
+          "app.help.support.text1" /* If you need more help come talk with us on WeakAuras's */
+        )
+      }}
+      <a href="https://discord.gg/weakauras" target="_blank">Discord</a>
+      {{
+        $t("app.help.support.text2" /* in the #companion-support channel. */)
+      }}
+    </div>
+    <br />
+    <br />
+  </div>
+</template>
+
+<style scoped lang="scss">
+#help {
+  padding: 10px 2.35vw;
+  text-align: left;
+  height: 100%;
+  position: relative;
+  overflow: auto;
+  font-weight: 500;
+}
+
+#help > ol {
+  margin-left: 15px;
+  padding-left: 0;
+}
+
+ol,
+ul {
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 15px;
+  margin-left: 30px;
+}
+
+.follow {
+  counter-reset: item;
+}
+
+.follow > li {
+  display: block;
+  margin-top: 5px;
+}
+
+.follow > li:before {
+  content: counters(item, ".") ". ";
+  counter-increment: item;
+}
+
+li > img {
+  transform: translate(0, 8px);
+}
+
+.center {
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 0);
+  margin: 20px 0 0;
+  max-width: 100%;
+}
+
+a {
+  color: rgb(255, 209, 0);
+  font-weight: 700;
+}
+
+a:hover {
+  color: rgb(255, 228, 106);
+}
+
+.reset {
+  margin-top: 0px !important;
+}
+
+.paragraph {
+  margin-left: 15px;
+  font-family: "Source Sans Pro", sans-serif;
+}
+</style>
---
+++ src/components/UI/StopMotionResult.vue
@@ -0,0 +1,194 @@
+<template>
+  <div id="StopMotionResult">
+    <img :src="preview" class="preview" />
+    <br />
+    <p class="center-text">
+      {{ $t("app.stopmotion.auraready" /*  WeakAuras is Ready For Install */) }}
+      <span
+        class="material-icons minihelp"
+        v-tooltip="{
+          content: rfiTooltip,
+          html: true,
+          strategy: 'fixed',
+          theme: 'info-tooltip',
+        }"
+        >help</span
+      >
+      <br /><br />
+      <span class="glow">{{
+        $t("app.stopmotion.restartwow2" /*  Restart World of Warcraft */)
+      }}</span>
+    </p>
+    <br />
+    <p class="filename">
+      <a
+        class="explorer"
+        @click="openDestDir()"
+        :title="$t('app.config.backup.openfolder' /* Open Folder */)"
+        >{{ resultFolder }}</a
+      >
+      <a
+        class="explorer"
+        @click="openDestFile()"
+        :title="$t('app.config.backup.openfile' /* Open File */)"
+        >{{ resultFile }}</a
+      >
+    </p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useStopMotionStore } from "../../stores/stopmotion";
+import { useStashStore } from "../../stores/auras";
+import path from "path";
+import { shell } from "electron";
+import {
+  StopMotionTemplate,
+  serialize,
+  deflate,
+  encode,
+  GenerateUniqueID,
+} from "../../libs/stopmotion";
+
+export default defineComponent({
+  name: "StopMotionResult",
+  setup() {
+    const { result, gif } = useStopMotionStore();
+    const stash = useStashStore();
+    return {
+      gif,
+      result,
+      stash,
+    };
+  },
+  mounted() {
+    this.stash.add({
+      slug: "StopMotion " + this.gif.meta.name,
+      name: "StopMotion " + this.gif.meta.name,
+      author: "WeakAuras Companion",
+      wagoVersion: "1",
+      wagoSemver: "1.0.0",
+      auraType: "WeakAuras",
+      addon: "WeakAuras",
+      source: "WeakAuras Companion",
+      encoded: this.weakauras_string,
+      logo: "Interface\\AddOns\\WeakAuras\\Media\\Textures\\logo_64_nobg.tga",
+    });
+    console.log(`added to stash ${this.gif.meta.name}`);
+  },
+  computed: {
+    weakauras_string() {
+      const SMtemplate = Object.assign({}, StopMotionTemplate);
+      SMtemplate.d.id = "StopMotion " + this.gif.meta.name;
+      SMtemplate.d.foregroundTexture = this.stopMotionInput;
+      SMtemplate.d.backgroundTexture = this.stopMotionInput;
+      SMtemplate.d.uid = GenerateUniqueID();
+
+      if (this.gif.tenor === true) {
+        SMtemplate.d.tenorID = this.gif.tenorID;
+      } else {
+        delete SMtemplate.d.tenorID;
+      }
+      const serialized = serialize(SMtemplate);
+      const compressed = deflate(serialized);
+      const encoded = encode(compressed);
+      return "!WA:1!" + encoded;
+    },
+    stopMotionInput() {
+      return path.join(
+        "Interface",
+        "AddOns",
+        "WeakAurasCompanion",
+        "animations",
+        path.parse(this.result.destination).base
+      );
+    },
+    resultFolder() {
+      return (
+        path.join("Interface", "AddOns", "WeakAurasCompanion", "animations") +
+        path.sep
+      );
+    },
+    resultFile() {
+      return path.parse(this.result.destination).base;
+    },
+    preview() {
+      return "data:image/png;base64, " + this.result.preview;
+    },
+  },
+  methods: {
+    openDestDir() {
+      shell.openPath(path.parse(this.result.destination).dir);
+    },
+    openDestFile() {
+      shell.openPath(this.result.destination);
+    },
+    rfiTooltip() {
+      // eslint-disable-next-line quotes
+      return `<img width="300px" src="../../assets/ready-for-install-example.png" />`;
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.btn-ok {
+  height: 35px;
+  span {
+    position: relative;
+    bottom: 8px;
+    line-height: 50px;
+  }
+}
+
+.minihelp {
+  font-size: 90%;
+}
+
+.btn-ok > * {
+  cursor: pointer;
+}
+
+.preview {
+  object-fit: contain;
+  width: 100%;
+  height: 300px;
+}
+
+.center-text {
+  text-align: center;
+}
+
+.glow {
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@keyframes glow {
+  from {
+    text-shadow: 0px 0px 5px #fff, 0px 0px 5px #614ad3;
+  }
+  to {
+    text-shadow: 0px 0px 20px #fff, 0px 0px 20px #614ad3;
+  }
+}
+
+.filename {
+  width: 100%;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.explorer {
+  cursor: pointer;
+  font-size: 12px;
+  margin-top: 5px;
+  color: rgb(255, 209, 0);
+  font-weight: 500;
+  &:hover {
+    text-decoration-line: underline;
+  }
+}
+</style>
