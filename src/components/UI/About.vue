39f3cae0b8bd8494d95a826f5d966bd50ceb630f
Benjamin Staneck <staneck@gmail.com>
2023-01-16 03:03:22 +0100

    restructure project

---
+++ src/components/UI/About.vue
@@ -0,0 +1,157 @@
+<template>
+  <div id="about">
+    <div id="main">
+      <h2>WeakAuras Companion</h2>
+      <br />
+      <p>
+        {{
+          $t(
+                "app.about.tools",
+            { tools: "Electron &amp; Vue" } /* Developed with {tools} */
+          )
+        }}<br />
+        {{
+          $t(
+                "app.about.version",
+            { version: version } /* Version: {version} */
+          )
+        }}<br />
+        {{
+          $t(
+                "app.about.license",
+            { license: "GPLv2" } /* License: {license} */
+          )
+        }}<br />
+        {{ $t("app.about.source" /* Sources on */) }}
+        <a href="https://github.com/WeakAuras/WeakAuras-Companion" target="_blank">GitHub</a><br />
+        {{ $t("app.about.author" /* Author */) }} Matthieu Saint Aubin aka
+        Buds<br />
+        {{ $t("app.about.platersupport" /* Plater support */) }}
+        Cont1nuity/Ariani<br />
+        {{ $t("app.about.cssby" /* CSS by */) }}
+        <a href="https://sragia.com/" target="_blank">Aigars Bedeicis aka Exality</a>
+        and <a href="https://stephanosue.com/" target="_blank">Stephano Sue</a>
+      </p>
+      <br />
+    </div>
+    <div id="links">
+      <div style="flex: 50%">
+        <div class="title">WeakAuras</div>
+        <div class="items">
+          <div v-for="media in medias.weakauras" :key="media.name" class="media-item">
+            <a :href="media.url" target="_blank">
+              <img :src="`../../src/assets/social-icons/${media.name}.svg`" class="logo" :title="media.name" />
+              {{ media.displayName }}
+            </a>
+          </div>
+        </div>
+      </div>
+      <div style="flex: 50%">
+        <div class="title">Wago</div>
+        <div class="items">
+          <div v-for="media in medias.wago" :key="media.name" class="media-item">
+            <a :href="media.url" target="_blank">
+              <img :src="`../../src/assets/social-icons/${media.name}.svg`" class="logo" :title="media.name" />
+              {{ media.displayName }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+import medias from "../../libs/contacts";
+import { version as VERSION } from "../../../package.json";
+
+export default defineComponent({
+  name: "About",
+  filters: {
+    capitalize: (string) => string.charAt(0).toUpperCase() + string.slice(1),
+  },
+  data() {
+    return {
+      medias,
+      // eslint-disable-next-line no-undef
+      version: VERSION,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+#about {
+  padding: 10px 2.35vw;
+  text-align: left;
+  height: 100%;
+  position: relative;
+  font-weight: 500;
+}
+
+#links a {
+  align-items: center;
+  vertical-align: middle;
+}
+
+#about img {
+  margin-right: 10px;
+  vertical-align: middle;
+  cursor: pointer;
+}
+
+#about>.valign {
+  flex: 50%;
+  align-items: center;
+}
+
+#main {
+  font-weight: 100;
+  text-align: center;
+  width: 100%;
+  margin: auto;
+}
+
+#links {
+  display: flex;
+  width: 450px;
+  margin: auto;
+  border-radius: 8px;
+  padding-bottom: 5px;
+}
+
+.media-item {
+  margin: 10px 15px;
+  cursor: pointer;
+}
+
+.logo {
+  opacity: 1;
+  width: 25px;
+  top: 0;
+}
+
+.title {
+  border-left: none;
+  border-bottom: 2px solid rgb(255, 209, 0);
+  border-radius: 8px 8px 0 0;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+a:hover {
+  color: rgb(255, 209, 0);
+}
+
+.fab {
+  margin-right: 5px;
+  font-size: 22px;
+  color: #e6e6e6 !important;
+}
+
+.items {
+  margin: 0 30px;
+}
+</style>
---
+++ src/index.ts
@@ -0,0 +1,108 @@
+import axios from "axios";
+import FloatingVue from "floating-vue";
+import { createPinia } from "pinia";
+import { createPersistedStatePlugin } from "pinia-plugin-persistedstate-2";
+import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import App from "./App.vue";
+import devtools from "@vue/devtools";
+import { ipcRenderer } from "electron";
+
+import de from "../i18n/de.json";
+import en from "../i18n/en.json";
+import es from "../i18n/es.json";
+import fr from "../i18n/fr.json";
+import ru from "../i18n/ru.json";
+import tr from "../i18n/tr.json";
+import zhcn from "../i18n/zh-cn.json";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+async function getStore(key) {
+  return await ipcRenderer.invoke("getStore", key);
+}
+
+async function setStore(key, value) {
+  return await ipcRenderer.invoke("setStore", key, value);
+}
+
+async function deleteStore(key) {
+  return await ipcRenderer.invoke("deleteStore", key);
+}
+
+pinia.use(
+  createPersistedStatePlugin({
+    storage: {
+      getItem: async (key) => {
+        return getStore(key);
+      },
+      setItem: async (key, value) => {
+        return setStore(key, value);
+      },
+      removeItem: async (key) => {
+        return deleteStore(key);
+      },
+    },
+  })
+);
+
+axios.defaults.timeout = 15000;
+app.config.globalProperties.$http = axios;
+// Vue.http = Vue.prototype.$http;
+
+app.use(pinia);
+
+const configStoreSerialized = getStore("configStore");
+
+let locale = "en";
+
+if (typeof configStoreSerialized === "string") {
+  locale = JSON.parse(configStoreSerialized).lang;
+}
+
+const i18n = createI18n({
+  locale: locale,
+  fallbackLocale: "en",
+  messages: {
+    en,
+    es,
+    fr,
+    de,
+    ru,
+    tr,
+    "zh-cn": zhcn,
+  },
+  pluralizationRules: {
+    ru: function (choice, choicesLength) {
+      if (choicesLength < 4) {
+        /* amount of available choices is incorrect (e.g. untranslated English phrase) */
+        return choice === 0 ? 0 /* none */ : choice !== 1 ? 2 /* everything else */ : 1; /* is 1 */
+      } else {
+        /* amount of available choices is correct */
+        return choice === 0
+          ? 0 /* none */
+          : choice % 10 === 1 && choice % 100 !== 11
+          ? 1 /* ends in 1, excluding 11 */
+          : choice % 10 >= 2 && choice % 10 <= 4 && (choice % 100 < 10 || choice % 100 >= 20)
+          ? 2 /* ends in 2-4, excluding 12-14 */
+          : 3; /* everything else */
+      }
+    },
+  },
+});
+
+app.use(i18n);
+
+app.use(FloatingVue, {
+  themes: {
+    "info-tooltip": {
+      $extend: "tooltip",
+    },
+  },
+});
+
+app.mount("#app").$nextTick(() => {
+  postMessage({ payload: "removeLoading" }, "*");
+});
+devtools.connect();
