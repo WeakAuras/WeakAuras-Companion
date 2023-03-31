f939fcedbe27d9c862e26e48cf4f9367b9241a06
Benjamin Staneck <staneck@gmail.com>
2023-03-31 18:47:07 +0200

    fix lints

--- tools/scripts/Utils.ts
+++ tools/scripts/Utils.ts
@@ -3,30 +3,32 @@ export const getTranslationsFromString = (content: string): RegExpMatchArray | s
 };
 
 export const sanitizeMessage = (message: string): string => {
-  const replacements: Array<{ from: string | RegExp, to: string }> = [
-    { from: /\s\s+/g, to: ' ' },
-    { from: '/*', to: '' },
-    { from: '*/', to: '' },
-    { from: /\[/g, to: '<' },
-    { from: /\]/g, to: '>' },
+  const replacements: Array<{ from: string | RegExp; to: string }> = [
+    { from: /\s\s+/g, to: " " },
+    { from: "/*", to: "" },
+    { from: "*/", to: "" },
+    { from: /\[/g, to: "<" },
+    { from: /\]/g, to: ">" },
     { from: /"/g, to: '\\"' },
   ];
 
-  replacements.forEach((replacement: { from: string | RegExp, to: string }) => {
+  replacements.forEach((replacement: { from: string | RegExp; to: string }) => {
     message = message.replace(replacement.from, replacement.to);
   });
 
   return message.trim();
 };
+
 export const getTranslationObject = (matches: string[]): any => {
   const translations: any = {};
 
   matches.forEach((translation: string): void => {
-    const id: string = translation.match(/["',]\S*["',]/)![0].replace(/[\\"',]/g, "");
+    const id = translation.match(/["',]\S*["',]/)?.[0].replace(/[\\"',]/g, "") as string;
     const defaultMessage: RegExpMatchArray | null = translation.match(/\/\*[^/]*\*\//);
+
     if (defaultMessage) {
-      const defaultMessageToSanitize = defaultMessage[0]
-      translations[id] = sanitizeMessage(defaultMessageToSanitize);
+      const defaultMessageToSanitize = defaultMessage[0];
+      translations[id] = sanitizeMessage(defaultMessageToSanitize) as string;
     }
   });
 
