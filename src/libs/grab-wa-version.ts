c35a32c3be95ea9a36157ebc4aa51fef292fe895
Benjamin Staneck <staneck@gmail.com>
2023-05-23 02:05:32 +0200

    Add fallback when TOC file cannot be scanned from file

--- src/libs/grab-wa-version.ts
+++ src/libs/grab-wa-version.ts
@@ -2,24 +2,47 @@ import fs from "fs";
 import path from "path";
 
 export function grabVersionFromToc(wowPath: string, version: string) {
+  const waFolderPath = path.join(wowPath, version, "Interface", "AddOns", "WeakAuras");
   const waTocFile = version.includes("classic")
-    ? path.join(wowPath, version, "Interface", "AddOns", "WeakAuras", "WeakAuras_Wrath.toc")
+    ? path.join(waFolderPath, "WeakAuras_Wrath.toc")
     : version.includes("era")
-    ? path.join(wowPath, version, "Interface", "AddOns", "WeakAuras", "WeakAuras_Vanilla.toc")
-    : path.join(wowPath, version, "Interface", "AddOns", "WeakAuras", "WeakAuras.toc");
+    ? path.join(waFolderPath, "WeakAuras_Vanilla.toc")
+    : path.join(waFolderPath, "WeakAuras.toc");
 
-  const tocContent = fs.readFileSync(waTocFile, "utf8");
-  const lines = tocContent.split("\n");
+  // TODO: Symlink handling
+  // const isSymlink = function(waFolderPath) {
+  //   try {
+  //     return fs.lstatSync(waFolderPath).isSymbolicLink();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //   return null;
+  // };
 
-  let waTocVersion = "";
+  // if (isSymlink) {
+  //   fs.readlink(path.join(waFolderPath), (err, linkString) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log("Path of the symlink:", linkString);
+  //     }
+  //   });
+  // }
 
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].includes("## Interface: ")) {
-      const parts = lines[i].split(" ");
-      waTocVersion = parts[2];
-      break;
+  if (fs.existsSync(waTocFile)) {
+    const tocContent = fs.readFileSync(waTocFile, "utf8");
+    const lines = tocContent.split("\n");
+
+    let waTocVersion = "";
+
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].includes("## Interface: ")) {
+        const parts = lines[i].split(" ");
+        waTocVersion = parts[2];
+        break;
+      }
     }
+    return waTocVersion;
   }
-
-  return waTocVersion;
+  return 100100; // Fallback
 }
