e9ae2d653ed1d031340a6c04888b57c897ee6dbb
Benjamin Staneck <staneck@gmail.com>
2023-05-26 05:43:22 +0200

    Refactor and more types

--- src/libs/wowstat.ts
+++ src/libs/wowstat.ts
@@ -1,32 +1,43 @@
-import fs from "fs";
+import fs, { Stats } from "fs";
 import path from "path";
 import { Tail } from "tail";
 
-const clientlog = {};
+interface VersionConfig {
+  name: string;
+  account: string;
+}
 
-export function isOpen(wowpath: string, version: string) {
+interface AfterReloadConfig {
+  value: string;
+  version: string;
+  versions: VersionConfig[];
+}
+
+type AfterReloadCallback = () => void;
+
+const clientlog: { [key: string]: Tail } = {};
+
+export async function isOpen(wowpath: string, version: string): Promise<boolean> {
   const logfile = path.join(wowpath, version, "Logs", "Client.log");
   const renametest = path.join(wowpath, version, "Logs", "Client.log.test");
 
   try {
-    fs.renameSync(logfile, renametest);
+    await fs.promises.rename(logfile, renametest);
+    await fs.promises.rename(renametest, logfile);
+    return false;
   } catch (err) {
     return true;
   }
-  fs.renameSync(renametest, logfile);
-  return false;
 }
 
-export function afterReload(config: { value: any; version: any; versions: any[] }, callback: () => void) {
-  const wowpath = config.value;
-  const version = config.version;
-  let account: string;
+export async function afterReload(config: AfterReloadConfig, callback: AfterReloadCallback) {
+  const { value: wowpath, version, versions } = config;
+  const versionConfig = versions.find((v) => v.name === version);
 
-  config.versions.forEach((version) => {
-    if (version.name === config.version) {
-      account = version.account;
-    }
-  });
+  if (!versionConfig) {
+    throw new Error(`Version configuration not found for version: ${version}`);
+  }
+  const { account } = versionConfig;
 
   const wacompanionsvfile = path.join(
     wowpath,
@@ -38,24 +49,25 @@ export function afterReload(config: { value: any; version: any; versions: any[] 
     "WeakAurasCompanion.lua"
   );
 
-  const { mtime } = fs.statSync(wacompanionsvfile);
-  let fsWait: NodeJS.Timeout | null = null;
+  let mtime: number;
 
-  const watcher = fs.watch(wacompanionsvfile, (event, filename) => {
-    if (filename) {
-      if (fsWait != null) {
-        return;
-      }
+  try {
+    const stats: Stats = await fs.promises.stat(wacompanionsvfile);
+    mtime = stats.mtimeMs;
+  } catch (err) {
+    throw new Error(`Failed to get file stats for: ${wacompanionsvfile}`);
+  }
 
-      fsWait = setTimeout(() => {
-        fsWait = null;
-      }, 100);
-      const stats = fs.statSync(wacompanionsvfile);
+  const watcher = fs.watch(wacompanionsvfile, { persistent: false }, async (event, filename) => {
+    if (!filename) {
+      return;
+    }
 
-      if (stats.mtime.valueOf() !== mtime.valueOf()) {
-        callback();
-        watcher.close();
-      }
+    const stats: Stats = await fs.promises.stat(wacompanionsvfile);
+
+    if (stats.mtimeMs !== mtime) {
+      watcher.close();
+      callback();
     }
   });
 }
@@ -69,7 +81,7 @@ export function afterRestart(wowpath: string, version: string, callback: () => v
     clientlog[version].watch();
   }
 
-  clientlog[version].on("line", (data) => {
+  clientlog[version].on("line", (data: string) => {
     const event = data.split(/ {2}/)[1];
 
     if (event === "Client Destroy") {
