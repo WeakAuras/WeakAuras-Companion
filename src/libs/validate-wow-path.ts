7a939dcc2af771e047310ae4f0b52c0892c9a79b
Benjamin Staneck <staneck@gmail.com>
2023-05-30 01:08:10 +0200

    Move validating the WoW path to its own file

---
+++ src/libs/validate-wow-path.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+
+import { buildAccountList } from "./build-account-list";
+import { buildVersionList } from "./build-version-list";
+
+import { ConfigState } from "@/stores/config";
+
+export function validateWowPath(config: ConfigState, versionOptions, accountOptions, versionSelected, auras) {
+  console.log("validateWowPath");
+  config.wowpath.validated = false;
+
+  if (config.wowpath.value) {
+    const wowPath = config.wowpath.value;
+    const dataFolder = path.join(wowPath, "Data");
+
+    // test if ${wowPath}\Data exists
+    try {
+      fs.accessSync(dataFolder, fs.constants.F_OK);
+      const files = fs.readdirSync(wowPath);
+      let validated = false;
+
+      files
+        .filter((versionDir) => versionDir.match(/^_.*_$/) && fs.statSync(path.join(wowPath, versionDir)).isDirectory())
+        .forEach((versionDir) => {
+          if (!validated) {
+            const accountFolder = path.join(wowPath, versionDir, "WTF", "Account");
+
+            if (fs.existsSync(accountFolder)) {
+              try {
+                fs.accessSync(accountFolder, fs.constants.F_OK);
+                validated = true;
+                config.wowpath.validated = true;
+                buildVersionList(config, versionOptions, accountOptions);
+                buildAccountList(config, accountOptions, versionSelected, auras);
+              } catch (err) {
+                console.error("No Read access");
+              }
+            }
+          }
+        });
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  }
+}
