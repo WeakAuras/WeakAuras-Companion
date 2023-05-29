d1fafea55229ce3adc282a6ffb0f8f9cbe7d0f9b
Benjamin Staneck <staneck@gmail.com>
2023-05-30 01:38:17 +0200

    Simplify grabbing SV files

--- src/libs/grab-sv-files.ts
+++ src/libs/grab-sv-files.ts
@@ -1,85 +1,44 @@
 import fs from "fs";
 import path from "path";
-import { ConfigState } from "@/stores/config";
 
-export function WeakAurasSaved(config?: ConfigState, version?, account?) {
-  let WeakAurasSavedVariable: fs.PathLike;
+import { Account, ConfigState, Version } from "@/stores/config";
 
+function getSavedVariable(config: ConfigState, version: string, accountName: string, addonName: string) {
+  const savedVariable = path.join(
+    config.wowpath.value,
+    version,
+    "WTF",
+    "Account",
+    accountName,
+    "SavedVariables",
+    `${addonName}.lua`
+  );
+
+  try {
+    fs.accessSync(savedVariable, fs.constants.F_OK);
+    return savedVariable;
+  } catch (e) {
+    console.log(`Error testing ${addonName} SV access\n${JSON.stringify(e)}`);
+    return false;
+  }
+}
+
+export function WeakAurasSaved(config?: ConfigState, version?: Version, account?: Account) {
   if (config?.wowpath?.version) {
-    const { version } = config.wowpath;
-
-    WeakAurasSavedVariable = path.join(
-      config.wowpath.value,
-      version,
-      "WTF",
-      "Account",
-      account.name,
-      "SavedVariables",
-      "WeakAuras.lua"
-    );
+    return getSavedVariable(config, config.wowpath.version, account?.name, "WeakAuras");
   } else if (config && version && account) {
-    const accountSelected = account.name;
-
-    WeakAurasSavedVariable = path.join(
-      config.wowpath.value,
-      config.wowpath.version,
-      "WTF",
-      "Account",
-      accountSelected,
-      "SavedVariables",
-      "WeakAuras.lua"
-    );
+    return getSavedVariable(config, version.name, account.name, "WeakAuras");
   }
 
-  if (WeakAurasSavedVariable) {
-    try {
-      fs.accessSync(WeakAurasSavedVariable, fs.constants.F_OK);
-      return WeakAurasSavedVariable;
-    } catch (e) {
-      console.log(`Error testing WeakAuras SV access\n${JSON.stringify(e)}`);
-      return false;
-    }
-  }
   return false;
 }
 
-export function PlaterSaved(config?: ConfigState, version?, account?) {
-  let PlaterSavedVariable: fs.PathLike;
-
+export function PlaterSaved(config?: ConfigState, version?: Version, account?: Account) {
   if (config?.wowpath?.version) {
-    const { version } = config.wowpath;
-
-    PlaterSavedVariable = path.join(
-      config.wowpath.value,
-      version,
-      "WTF",
-      "Account",
-      account.name,
-      "SavedVariables",
-      "Plater.lua"
-    );
+    return getSavedVariable(config, config.wowpath.version, account?.name, "Plater");
   } else if (config && version && account) {
-    const accountSelected = account.name;
-
-    PlaterSavedVariable = path.join(
-      config.wowpath.value,
-      config.wowpath.version,
-      "WTF",
-      "Account",
-      accountSelected,
-      "SavedVariables",
-      "Plater.lua"
-    );
+    return getSavedVariable(config, version.name, account.name, "Plater");
   }
 
-  if (PlaterSavedVariable) {
-    try {
-      fs.accessSync(PlaterSavedVariable, fs.constants.F_OK);
-      return PlaterSavedVariable;
-    } catch (e) {
-      console.log(`Error testing Plater SV access\n${JSON.stringify(e)}`);
-      return false;
-    }
-  }
   return false;
 }
