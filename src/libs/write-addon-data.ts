2c1cb2e14974e311c68b3f81258b3fb414f1dff4
Benjamin Staneck <staneck@gmail.com>
2023-06-10 19:36:02 +0200

    Fix init.lua data writing
    Broke in 94e2afacdb6459af02752f46c1c3d99470c119c6

--- src/libs/write-addon-data.ts
+++ src/libs/write-addon-data.ts
@@ -172,6 +172,38 @@ init.lua`;
         }
       });
     });
+
+    const initTemplateData = `-- file generated automatically
+local loadedFrame = CreateFrame("FRAME")
+loadedFrame:RegisterEvent("ADDON_LOADED")
+loadedFrame:SetScript("OnEvent", function(_, _, addonName)
+  if addonName == "WeakAurasCompanion" then
+    if WeakAuras and WeakAuras.AddCompanionData and WeakAurasCompanionData then
+      local WeakAurasData = WeakAurasCompanionData.WeakAuras
+      if WeakAurasData then
+        WeakAuras.AddCompanionData(WeakAurasData)
+        WeakAuras.StopMotion.texture_types["WeakAuras Companion"] = WeakAuras.StopMotion.texture_types["WeakAuras Companion"] or {}
+        local CompanionTextures = WeakAuras.StopMotion.texture_types["WeakAuras Companion"]
+        for fileName, name in pairs(WeakAurasData.stopmotionFiles) do
+          CompanionTextures["Interface\\\\AddOns\\\\WeakAurasCompanion\\\\animations\\\\" .. fileName] = name
+        end
+      end
+    end
+
+    if Plater and Plater.AddCompanionData and WeakAurasCompanionData and WeakAurasCompanionData.Plater then
+      Plater.AddCompanionData(WeakAurasCompanionData.Plater)
+    end
+  end
+end)
+`;
+
+    const filePath = path.join(addonFolder, "init.lua");
+
+    fs.writeFile(filePath, initTemplateData, (err) => {
+      if (err) {
+        throw new Error("errorFileSave");
+      }
+    });
   }
   backup(config, addonsInstalled);
 }
