caf9d639a7a1d3bfbb320c54988c8a30e69d2d19
Benjamin Staneck <staneck@gmail.com>
2023-12-08 04:14:28 +0100

    Update spinner

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -1,4 +1,6 @@
-function domReady(condition: DocumentReadyState[] = ["complete", "interactive"]) {
+function domReady(
+  condition: DocumentReadyState[] = ["complete", "interactive"],
+) {
   return new Promise((resolve) => {
     if (condition.includes(document.readyState)) {
       resolve(true);
@@ -14,12 +16,12 @@ function domReady(condition: DocumentReadyState[] = ["complete", "interactive"])
 
 const safeDOM = {
   append(parent: HTMLElement, child: HTMLElement) {
-    if (!Array.from(parent.children).find(e => e === child)) {
+    if (!Array.from(parent.children).find((e) => e === child)) {
       return parent.appendChild(child);
     }
   },
   remove(parent: HTMLElement, child: HTMLElement) {
-    if (Array.from(parent.children).find(e => e === child)) {
+    if (Array.from(parent.children).find((e) => e === child)) {
       return parent.removeChild(child);
     }
   },
@@ -34,18 +36,18 @@ const safeDOM = {
 function useLoading() {
   const className = "loaders-css__square-spin";
   const styleContent = `
-@keyframes square-spin {
-  25% { transform: perspective(100px) rotateX(180deg) rotateY(0); }
-  50% { transform: perspective(100px) rotateX(180deg) rotateY(180deg); }
-  75% { transform: perspective(100px) rotateX(0) rotateY(180deg); }
-  100% { transform: perspective(100px) rotateX(0) rotateY(0); }
+@keyframes pulse {
+  0% { opacity: 0.5; }
+  50% { opacity: 1; }
+  100% { opacity: 0.5; }
 }
 .${className} > div {
   animation-fill-mode: both;
-  width: 50px;
-  height: 50px;
-  background: #fff;
-  animation: square-spin 3s 0s cubic-bezier(0.09, 0.57, 0.49, 0.9) infinite;
+  width: 225px;
+  height: 75px;
+  background: url('src/assets/weakauras.png');
+  background-size: cover;
+  animation: pulse 3s 0s cubic-bezier(0.09, 0.57, 0.49, 0.9) infinite;
 }
 .app-loading-wrap {
   position: fixed;
@@ -56,7 +58,7 @@ function useLoading() {
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #282c34;
+  background: #131313;
   z-index: 9;
 }
     `;
