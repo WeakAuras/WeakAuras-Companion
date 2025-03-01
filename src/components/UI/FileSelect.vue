42b1e40708806fd57786c48c10505afa0178c450
Benjamin Staneck <staneck@gmail.com>
2025-03-01 02:37:07 +0100

    refactor: move FileSelect component to composition api

--- src/components/UI/FileSelect.vue
+++ src/components/UI/FileSelect.vue
@@ -1,100 +1,72 @@
-<script lang="ts">
+<script setup lang="ts">
 import { ipcRenderer } from "electron";
-import { defineComponent } from "vue";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "FileSelect",
-  props: {
-    path: {
-      type: String,
-      default: "",
-      required: false,
-    },
-    createDirectory: {
-      type: Boolean,
-      default: false,
-    },
-    defaultPath: {
-      type: String,
-      default: "",
-    },
-    openDirectory: {
-      type: Boolean,
-      default: false,
-    },
-    openFile: {
-      type: Boolean,
-      default: false,
-    },
-    filters: {
-      type: Array,
-      default: () => [],
-    },
-    dragndrop: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  emits: ["update:path"],
-  data() {
-    return {
-      dialogOpen: false,
+const props = defineProps<{
+  path?: string;
+  createDirectory?: boolean;
+  defaultPath?: string;
+  openDirectory?: boolean;
+  openFile?: boolean;
+  filters?: Array<any>;
+  dragndrop?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:path", path: string): void;
+}>();
+
+const dialogOpen = ref(false);
+
+async function handleInputClick() {
+  if (!dialogOpen.value) {
+    dialogOpen.value = true;
+
+    const dialogOptions = {
+      properties: [] as string[],
+      defaultPath: props.path || props.defaultPath || "",
+      filters: [] as any[],
     };
-  },
-  methods: {
-    async handleInputClick() {
-      if (!this.dialogOpen) {
-        this.dialogOpen = true;
 
-        const dialogOptions = {
-          properties: [],
-          defaultPath: this.path || this.defaultPath,
-          filters: [],
-        };
+    if (props.openDirectory) {
+      dialogOptions.properties.push("openDirectory");
+    }
 
-        if (this.openDirectory) {
-          dialogOptions.properties.push("openDirectory");
-        }
+    if (props.openFile) {
+      dialogOptions.properties.push("openFile");
+    }
 
-        if (this.openFile) {
-          dialogOptions.properties.push("openFile");
-        }
+    if (props.createDirectory) {
+      dialogOptions.properties.push("createDirectory");
+    }
 
-        if (this.createDirectory) {
-          dialogOptions.properties.push("createDirectory");
-        }
+    if (props.filters) {
+      dialogOptions.filters = props.filters;
+    }
 
-        if (this.filters) {
-          dialogOptions.filters = this.filters;
-        }
-
-        ipcRenderer
-          .invoke("openDialog", dialogOptions)
-          .then((result) => {
-            if (result.filePaths && result.filePaths.length) {
-              this.$emit("update:path", result.filePaths[0]);
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-        this.dialogOpen = false;
+    try {
+      const result = await ipcRenderer.invoke("openDialog", dialogOptions);
+      if (result.filePaths && result.filePaths.length) {
+        emit("update:path", result.filePaths[0]);
       }
-    },
-    drop(event) {
-      if (this.dragndrop === true) {
-        event.preventDefault();
+    } catch (error) {
+      console.error(error);
+    }
+    dialogOpen.value = false;
+  }
+}
 
-        this.$emit("update:path", event.dataTransfer.files[0].path);
-      }
-    },
-  },
-});
+function drop(event: DragEvent) {
+  if (props.dragndrop === true && event.dataTransfer?.files[0]) {
+    event.preventDefault();
+    emit("update:path", event.dataTransfer.files[0].path);
+  }
+}
 </script>
 
 <template>
   <div
-    v-if="dragndrop"
+    v-if="props.dragndrop"
     class="cursor-pointer border-2 border-brand-accent rounded-md border-dotted p-5 pl-50 pr-50 text-center"
     @dragenter.prevent
     @dragover.prevent
@@ -121,7 +93,7 @@ export default defineComponent({
       >
         <span
           class="wow-path table-cell overflow-hidden text-ellipsis align-middle"
-          >{{ path }}</span
+          >{{ props.path }}</span
         >
       </div>
       <i class="i-mdi-settings ml-1 mt-3 cursor-pointer text-2xl">settings</i>
